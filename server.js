619fd10f4b2ab691e053094652f091e12b848b9c
IamFurina <emotionui@qq.com>
2025-02-26 21:11:15 +0800

    Fix: 修复API不正常工作

--- server.js
+++ server.js
@@ -248,7 +248,6 @@ async function consturctServer(moduleDefs) {
               const { url } = await match(req.query.id, source)
               song.url = url
               song.freeTrialInfo = null // 对于Splayer来说，去除开通会员提示
-              status.playUseOtherSource = true
             }
         }
 
